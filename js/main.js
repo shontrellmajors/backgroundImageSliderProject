37f6744692871209fc6f6d0275c2484f4f276418
Shontrell Majors <smajors@pivottechschool.com>
2020-08-20 20:43:10 -0500

    updated main.js

--- js/main.js
+++ js/main.js
@@ -1,34 +1,37 @@
-//Immediately invoke function expression
-
-function() {
+// Immediately invoke function expression
+(function() {
     const pictures = [
-        "BLM1.jpg",
-        "BLM2.jpg",
-        "BLM3.jpg",
-        "BLM4.jpg",
-        "BLM5.jpg"
+        "BLM1",
+        "BLM2",
+        "BLM3",
+        "BLM4",
+        "BLM5"
     ];
     //select both left and right button. Add event listeners
-    const buttons = document.querySelectorAll(".btn")
-    const imageDIV = document.querySelector(".img-container")
+    const buttons = document.querySelectorAll('.btn')
+    const imageDIV = document.querySelector('.img-container')
     let counter = 0
-
     buttons.forEach(function(button){
-        button.addEventListener("click", function(e) {
-            if (button.classList.contains("btn-left")){
+        button.addEventListener('click', function(e){
+            if (button.classList.contains('btn-left')){
                 counter--
                 if(counter < 0){
                     counter = pictures.length -1
                 }
-                imageDIV.style.backgroundImage = "url("./img/${pictures[counter]}.jpg")
+                imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpg')`
             }
-            if (button.classList.contains("btn-right")) {
+            if (button.classList.contains('btn-right')) {
                 counter++
                 if(counter > pictures.length -1) {
                     counter = 0
                 }
-                imageDIV.style.backgroundImage = "url("./img/${pictures[counter]}.jpg")
+                imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpg')`
             }
         })
     })
 })();
+
+
+
+
+
